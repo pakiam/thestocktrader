7997b58325dbaa3a1c4e2163db5b5bb99cb6ba16
Maksim Home <akiam@mail.ru>
2017-11-01 21:04:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# vue-cli
+
+> A Vue.js project
+>
+> Before start create FireBase and link it in main.js
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        Trade or View your Portfolio
+        <h6>You may Save & Load your Data</h6>
+        <h6>Click 'End Day' to begin a New Day!</h6>
+        <hr>
+        <p>Your Funds: {{ funds | currency }}</p>
+    </div>
+</template>
+
+<script>
+export default{
+	computed: {
+		funds(){
+			return this.$store.getters.funds;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
